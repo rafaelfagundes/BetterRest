8f1deb6a42e0879a933904e9fb981c9984e768ae
Rafael Fagundes <rafaelcflima@gmail.com>
2024-09-01 20:24:45 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rafael Fagundes on 2024-09-01.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
